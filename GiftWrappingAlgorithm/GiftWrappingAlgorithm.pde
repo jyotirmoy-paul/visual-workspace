feb3598b85b48865feb70845e5de611bba39b542
Jyotirmoy Paul <cipherresfeber@gmail.com>
2019-07-31 23:35:55 +0530

    GiftWrappingAlgorithm -- randomly initializing points and displaying them

--- GiftWrappingAlgorithm/GiftWrappingAlgorithm.pde
+++ GiftWrappingAlgorithm/GiftWrappingAlgorithm.pde
@@ -1,9 +1,30 @@
-// implementing gift wrapping algorithm (convex hull) -- visualizing
+// implementing gift wrapping algorithm (convex hull/convex envelope) -- visualizing in 2D
+
+import java.util.Random;
+
+final int numberOfPoints = 10;
+PVector[] points = new PVector[numberOfPoints];
 
 void setup(){
+  size(600,400);
+  background(255);
+
+  // initialize the program with N random points
+  Random random = new Random();
+  for(int i=0; i<numberOfPoints; i++){
+    points[i] = new PVector(50 + random.nextInt(width-100), 50 + random.nextInt(height-100));
+  }
 
 }
 
-void draw(){
+void drawAllPoints(){
+  stroke(0);
+  strokeWeight(10);
+  for(PVector p: points){
+    point(p.x, p.y);
+  }
+}
 
+void draw(){
+  drawAllPoints();
 }
--- GiftWrappingAlgorithm/build/source/GiftWrappingAlgorithm.java
+++ GiftWrappingAlgorithm/build/source/GiftWrappingAlgorithm.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import java.util.Random; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -14,15 +16,37 @@ import java.io.IOException;
 
 public class GiftWrappingAlgorithm extends PApplet {
 
-// implementing gift wrapping algorithm (convex hull) -- visualizing
+// implementing gift wrapping algorithm (convex hull/convex envelope) -- visualizing in 2D
+
+
+
+final int numberOfPoints = 10;
+PVector[] points = new PVector[numberOfPoints];
 
 public void setup(){
+  
+  background(255);
 
+  // initialize the program with N random points
+  Random random = new Random();
+  for(int i=0; i<numberOfPoints; i++){
+    points[i] = new PVector(50 + random.nextInt(width-100), 50 + random.nextInt(height-100));
+  }
+
+}
+
+public void drawAllPoints(){
+  stroke(0);
+  strokeWeight(10);
+  for(PVector p: points){
+    point(p.x, p.y);
+  }
 }
 
 public void draw(){
-
+  drawAllPoints();
 }
+  public void settings() {  size(600,400); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GiftWrappingAlgorithm" };
     if (passedArgs != null) {
