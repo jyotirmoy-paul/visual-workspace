33f49a315b34863cbaf74239cda092bb52219697
Jyotirmoy Paul <cipherresfeber@gmail.com>
2019-07-31 21:41:46 +0530

    ThrowingDarts -- prepared the stage for the show -- setup function done

--- ThrowingDarts/ThrowingDarts.pde
+++ ThrowingDarts/ThrowingDarts.pde
@@ -1,5 +1,30 @@
-void setup(){
+import java.util.Random;
 
+final int radius = 150;
+int side = radius*2;
+
+long point_count_inside_circle;
+long total_point_count;
+
+Random random;
+
+void setup(){
+  background(255);
+  size(400, 400);
+
+  translate(width/2, height/2);
+
+  // draw the square -- side-length = side
+  rectMode(CENTER);
+  strokeWeight(1);
+  stroke(0);
+  noFill();
+  rect(0, 0, side, side);
+
+  // draw the circle -- radius = side/2
+  ellipse(0, 0, side, side);
+
+  random = new Random();
 }
 
 void draw(){
--- ThrowingDarts/build/source/ThrowingDarts.java
+++ ThrowingDarts/build/source/ThrowingDarts.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import java.util.Random; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -14,13 +16,46 @@ import java.io.IOException;
 
 public class ThrowingDarts extends PApplet {
 
-public void setup(){
 
+
+final int radius = 150;
+int side = radius*2;
+
+long point_count_inside_circle;
+long total_point_count;
+
+Random random;
+
+public void setup(){
+  background(255);
+  
+
+  translate(width/2, height/2);
+
+  // draw the square -- side-length = side
+  rectMode(CENTER);
+  strokeWeight(1);
+  stroke(0);
+  noFill();
+  rect(0, 0, side, side);
+
+  // draw the circle -- radius = side/2
+  ellipse(0, 0, side, side);
+
+  random = new Random();
 }
 
 public void draw(){
 
+  translate(width/2, height/2);
+
+  int x = random.nextInt(side);
+  int y = random.nextInt(side);
+
+  point(x, y);
+
 }
+  public void settings() {  size(400, 400); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "ThrowingDarts" };
     if (passedArgs != null) {
