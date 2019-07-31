e6a55d7c138ad622dcc35bf786d7cf5447a4f511
Jyotirmoy Paul <cipherresfeber@gmail.com>
2019-07-31 23:13:41 +0530

    GiftWrappingAlgorithm -- initial commit

---
+++ GiftWrappingAlgorithm/GiftWrappingAlgorithm.pde
@@ -0,0 +1,9 @@
+// implementing gift wrapping algorithm (convex hull) -- visualizing
+
+void setup(){
+
+}
+
+void draw(){
+
+}
---
+++ GiftWrappingAlgorithm/build/source/GiftWrappingAlgorithm.java
@@ -0,0 +1,34 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class GiftWrappingAlgorithm extends PApplet {
+
+// implementing gift wrapping algorithm (convex hull) -- visualizing
+
+public void setup(){
+
+}
+
+public void draw(){
+
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "GiftWrappingAlgorithm" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
